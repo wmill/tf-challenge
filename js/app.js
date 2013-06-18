323f0c5594a32d4aeb6f29751f78681aa0740f82
Walter Miller <walter@waltermiller.ca>
2013-06-18 00:25:11 -0400

    More progress

--- js/app.js
+++ js/app.js
@@ -10,7 +10,27 @@ app.controller("WidgetsController", function ($scope, Schedule){
 
 app.controller("ScheduleController", function ($scope, Schedule){
 	$scope.schedule = Schedule.schedule;
-})
+	$scope.title = "Scheduler";
+	$scope.addWidget = function(dayId){
+
+		//current_day = day for day in $scope.schedule.days when day.dayId == dayId
+		var current_day, day, _i, _len, _ref;
+
+		_ref = $scope.schedule.days;
+		for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+			day = _ref[_i];
+			if (day.dayId === dayId) {
+				current_day = day;
+			}
+		}
+		console.log(current_day);
+	};
+
+	$scope.addDay = function(){
+		Schedule.addDay();
+
+	}
+});
 
 app.factory('Schedule', function(){
 	var schedule = {
@@ -38,5 +58,19 @@ app.factory('Schedule', function(){
 			}
 		]
 	};
-	return {'schedule': schedule};
+
+	var addDay = function() {
+		var max_day = _.max(schedule.days, function(day){
+			return day.dayId;
+		});
+		schedule.days.push({
+			dayId: max_day.dayId + 1,
+			dayWidget: []
+		});
+	};
+
+	return {
+		'schedule': schedule,
+		'addDay': addDay
+	};
 });
