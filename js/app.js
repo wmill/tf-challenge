62ef6f7393bc01c51e2524b7c7d1f60202b217ad
Walter Miller <walter@waltermiller.ca>
2013-06-17 21:00:24 -0400

    Data now loading from a service

--- js/app.js
+++ js/app.js
@@ -1,2 +1,42 @@
-var app = angular.module("", []);
+var app = angular.module('scheduler', []);
 
+app.controller("DaysController", function ($scope, Schedule){
+
+});
+
+app.controller("WidgetsController", function ($scope, Schedule){
+
+});
+
+app.controller("ScheduleController", function ($scope, Schedule){
+	$scope.schedule = Schedule.schedule;
+})
+
+app.factory('Schedule', function(){
+	var schedule = {
+		name: 'My Project',
+		days: [
+			{
+				dayId: 1,
+				dayWidget: [
+					{widgetId: 1, widgetName: "Foo"},
+					{widgetId: 2, widgetName: "Bar"},
+					{widgetId: 3, widgetName: "Baz"}
+				]
+			},
+			{
+				dayId: 2,
+				dayWidget: [
+				]
+			},
+			{
+				dayId: 3,
+				dayWidget: [
+					{widgetId: 1, widgetName: "Foo"},
+					{widgetId: 2, widgetName: "Bar"}
+				]
+			}
+		]
+	};
+	return {'schedule': schedule};
+});
