4b570386d598282eb3f187403f3970890adb7af7
Walter Miller <walter@waltermiller.ca>
2013-06-19 12:52:59 -0400

    Drag drop almost working

--- js/app.js
+++ js/app.js
@@ -23,7 +23,74 @@ app.controller("ScheduleController", function ($scope, Schedule){
 		Schedule.addDay();
 
 	};
+
+	$scope.moveWidget = function(dayId, widgetId){
+		//remove the old widget
+		var widget;
+		for (var i=0; i < $scope.schedule.days.length; i++){
+			//remove the widget from the day, but keep the object
+			//var day = $scope.schedule.days[i];
+			for (var j=0; j < $scope.schedule.days[i].dayWidget.length; j++){
+				var target = $scope.schedule.days[i].dayWidget[j].widgetId;
+				if (target === widgetId){
+					widget = target;
+					console.log($scope.schedule.days[i].dayWidget, j)
+					$scope.schedule.days[i].dayWidget.remove(j);
+					console.log($scope.schedule.days[i].dayWidget, j)
+				}
+			} 
+		}
+
+		var day = _.find($scope.schedule.days, function(day){
+			return day.dayId === dayId;
+		});
+		console.log(day);
+		day.dayWidget.push(widget);
+		$scope.$apply();
+	}
+
+
+	
 });
+
+app.directive('draggablewidget', function(){
+	return {
+		scope: true,
+		restrict: 'A',
+		controller: function($scope, $element, $attrs, $location){
+			$element.attr('draggable', true);
+			$element.on('dragstart', function(e) {
+				console.log('dragstart:' + $attrs.draggablewidget);
+				e.originalEvent.dataTransfer.effectAllowed = 'move';
+				e.originalEvent.dataTransfer.setData('text/plain', JSON.stringify({
+					widgetId: $attrs.draggablewidget
+				}));
+			});
+
+		}
+	};
+});
+
+app.directive('recieveswidget', function(){
+	return {
+		restrict: 'A',
+		scope: true,
+		controller: function($scope, $element, $attrs, $location) {
+			
+			$element.on('dragover', function(e){
+				e.preventDefault();
+			});
+			$element.on('drop', function(e){
+				var dayId = parseInt($attrs.recieveswidget,10);
+				var data = JSON.parse(e.originalEvent.dataTransfer.getData('text/plain'));
+				console.log(data, dayId);
+				e.preventDefault();
+				$scope.moveWidget(dayId, data.widgetId);
+			});
+		}
+	}
+})
+
 
 app.directive("widget", function(){
 	return {
@@ -39,6 +106,7 @@ app.directive("widget", function(){
 				$scope.ctrlrFn($scope.day,$scope.widgetName);
 				$scope.widgetName = "";
 			};
+
 		}
 	};
 });
@@ -88,7 +156,7 @@ app.factory('Schedule', function(){
 });
 
 
-//helper function
+//helper functions
 function s4() {
   return Math.floor((1 + Math.random()) * 0x10000)
              .toString(16)
@@ -99,3 +167,10 @@ function guid() {
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
          s4() + '-' + s4() + s4() + s4();
 }
+
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
